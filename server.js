36fd38ea62dac708a1f50afc43dff3cb3f31b077
highgoal215 <highgoal215@gmail.com>
2025-07-15 12:02:21 +0900

    feat:change_sockectio, server_cors config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const httpServer = createServer(app);
 // Socket.IO setup
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://likes.io",
     methods: ["GET", "POST"]
   }
 });
@@ -61,8 +61,10 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: true,
+    origin: ["https://likes.io", "http://likeio.duckdns.org:9000", "http://likeio.duckdns.org:3000", "http://likeio.duckdns.org:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
   })
 );
 
